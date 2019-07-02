abd34ee587c344d4946ff2fb4b121d46477da601
Frank Lyder Bredland <fl@nyggen.com>
2019-07-02 16:56:18 +0200

    Command line tool

---
+++ Readme.md
@@ -0,0 +1,4 @@
+# HeliumAnion
+
+HeliumAnion is a replica of the Helium app for Mac, implemented using Electron.
+It comes with a simple rest api and command line tool to power it.
--- src/main.js
+++ src/main.js
@@ -1,25 +1,76 @@
 const { app, BrowserWindow } = require('electron'),
-		path = require('path');
+		path = require('path'),
+		express = require('express'),
+		bodyParser = require('body-parser');
 
-function createWindow () {
+const port = 2020;
+
+const createWindow = (url) => {
 	let win = new BrowserWindow({
 		width: 800,
 		height: 600,
 		webPreferences: {
 			nodeIntegration: true
-		}
+		},
 	});
 
 	// For some reasons you need to hide it for the settings under to take effect
 	// ref: https://github.com/electron/electron/issues/10078#issuecomment-386908380
 	app.dock.hide();
-	win.setAlwaysOnTop(true, 'floating')
+	win.setAlwaysOnTop(true, 'floating');
 	win.setVisibleOnAllWorkspaces(true, true);
 	win.setFullScreenable(false);
 	app.dock.show();
 
-	win.loadFile(path.join(__dirname, 'index.html'));
+	if(!url) {
+		win.loadFile(path.join(__dirname, 'index.html'));
+	} else {
+		win.loadURL(url);
+	}
 };
 
-app.on('ready', createWindow);
+const appOnPromise = (eventType) => new Promise(resolve =>
+	app.on(eventType, resolve)
+);
+
+const main = async () => {
+	await appOnPromise('ready');
+
+	const app = express();
+
+	app.use(bodyParser.json());
+	app.use(bodyParser.urlencoded({extended: true}));
+
+	app.get('/api/status', (req, res) => {
+		res.send({
+			'status': 'ok'
+		});	
+	});
+
+	app.post('/api/createWindow', (req, res) => {
+		createWindow();
+		res.send({
+			'message': 'new window created'
+		});
+	});
+
+	app.post('/api/open', (req, res) => {
+		if(!req.is('application/json')) {
+			return res.status(400).send({
+				"message": "You need to set content type to application/json"
+			});
+		}
+
+		createWindow(req.body.url);
+		res.send({
+			'message': 'new window created'
+		});
+	});
+
+
+	await app.listen(port);
+	console.log(`Server api started on ${port}`);
+};
+
+main();
 
